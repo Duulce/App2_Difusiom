0f0708e9dc5f31854a98c4ffd09ffe4c28646108
unknown <sh.dulce17@gmail.com>
2023-10-09 16:47:57 -0600

    Json

---
+++ src/servicio.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient}from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class ServicioService{
+    URL="assets/Datos/del_ahorro.json";
+
+    constructor(private http:HttpClient){
+
+    }
+get(){
+    return this.URL;
+}
+
+
+}
